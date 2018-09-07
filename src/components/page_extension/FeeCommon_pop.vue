d190d3ccfa8833bdd87c799a95bb4160a8ec0f4d
kevin <princecharmingthi@163.com>
2018-09-07 16:41:58 +0800

    费用

--- src/components/page_extension/FeeCommon_pop.vue
+++ src/components/page_extension/FeeCommon_pop.vue
@@ -1,5 +1,78 @@
 <template>
-  
+  <el-dialog :title=title :visible.sync="dialogVisible" class="dialog-small" @close="$refs.form.resetFields();">
+		<el-form :model="form" ref="form" label-width="80px" >
+			<el-form-item label="相关案件" prop="project" class="is-required">
+				<remote-select type="patent" v-model="form.project"></remote-select>
+			</el-form-item>
+			<el-form-item label="费用对象" prop="target" class="is-required">
+				<remote-select type="member" v-model="form.target"></remote-select>
+			</el-form-item>
+			
+			<el-form-item label="费用状态" prop="status" v-if="type == 'add'" class="is-required">
+				<fee-status v-model="form.status" :feeAnnual="feeAnnual"></fee-status>
+			</el-form-item>
+
+			<el-form-item label="费用代码" prop="code" class="is-required">
+				<static-select type="fee_code" v-model="form.code" ref="fee_code"></static-select>
+			</el-form-item>
+
+			<el-form-item label="费用金额" prop="money" class="is-required">
+				<el-row>					
+					<el-col :span="16" style="padding-right: 5px;">
+						<el-input v-model="form.money.amount" placeholder="请输入费用金额">
+							<el-select 
+								v-model="form.money.currency" 
+								slot="prepend" 
+								placeholder="请选择货币类型"
+								style="width: 150px;"
+							>
+								<el-option v-for="item in options.currencyType" :key="item.value" :label="item.label" :value="item.value"></el-option>
+							</el-select>
+						</el-input>
+					</el-col>
+
+					<el-col :span="8" style="padding-left: 5px;">
+						<el-input v-model="form.money.roe" placeholder="请输入执行汇率">
+							<template slot="prepend">执行汇率</template>
+						</el-input>
+					</el-col>
+
+				</el-row>
+			</el-form-item>
+			
+			<el-row>
+				<el-col :span="12">
+					<el-form-item label="费用期限" prop="due_time" class="is-required">
+						<el-date-picker v-model="form.due_time" type="date" placeholder="请选择费用期限"></el-date-picker>
+					</el-form-item>					
+				</el-col>
+				<el-col :span="12">
+					<el-form-item label="官方绝限" prop="deadline">
+						<el-date-picker v-model="form.deadline" type="date" placeholder="请选择官方期限"></el-date-picker>
+					</el-form-item>	
+				</el-col>
+			</el-row>
+
+			<el-form-item label="支付时间" prop="pay_time">
+				<el-date-picker v-model="form.pay_time" type="date" style="width: 100%;" placeholder="请选择支付时间"></el-date-picker>
+			</el-form-item>
+
+			
+			
+
+<!-- 			<el-form-item label="开票主体" prop="invoice_entity_id">
+				<invoice-entity v-model="form.invoice_entity_id"></invoice-entity>
+			</el-form-item> -->
+			<el-form-item label="备注" prop="remark">
+				<el-input type="textarea" placeholder="请填写备注" v-model="form.remark"></el-input>
+			</el-form-item>
+			<el-form-item style="margin-bottom: 0px">
+				<el-button type="primary" @click="add" v-if="type == 'add'" :loading="loading">{{ loading ? '添加中...' : '添加' }}</el-button>
+				<el-button type="primary" @click="edit" v-else-if="type == 'edit'" :loading="loading">{{ loading ? '编辑中...' : '编辑' }}</el-button>
+				<el-button @click="cancel" :disabled="loading">取消</el-button>
+			</el-form-item>
+		</el-form>
+	</el-dialog>
 </template>
 
 <script>
