43b647dde5c2915a1f27150d08d21d39c3252fad
Linxi <lx02321@163.com>
2018-10-26 15:32:26 +0800

    CRM模块客户管理联系人删除相关

--- src/components/page_extension/CustomerListContact.vue
+++ src/components/page_extension/CustomerListContact.vue
@@ -84,7 +84,7 @@
             },
             clientDelete(row) {
 
-                const url = `${URL}/${this.customer.id}/contacts/${row.id}`;
+                const url = `/contacts?id[]=${row.id}`;
                 this.$confirm(
                     '此操作将永久删除该信息, 是否继续?', '提示', {
                         confirmButtonText: '确定',
