d7da1d9b8c23c41878a2165bfb422f59516f67e5
kevin <princecharmingthi@163.com>
2019-01-14 20:45:34 +0800

    延期附件

--- src/components/page_extension/TaskCommon_delay.vue
+++ src/components/page_extension/TaskCommon_delay.vue
@@ -1,5 +1,10 @@
 <template v-loading="loading">
-  <app-table :columns="columns" :data="delayData"></app-table>
+  <div>
+    <app-table :columns="columns" :data="delayData"></app-table>
+    <el-dialog title="附件" :visible.sync="dialogVisible">
+      <app-table :columns="attachmentsColumns" :data="attachments"></app-table>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -14,6 +19,8 @@ export default {
     return {
       delayData: [],
       loading: false,
+      dialogVisible: false,
+      attachments: [],
       columns: [
         { type: "text", label: "操作人", prop: "days" },
         { type: "text", label: "申请时间", prop: "creation_time" },
@@ -21,10 +28,28 @@ export default {
         { type: "text", label: "审批人", prop: "reviewer",render_simple:'name' },
         { type: "text", label: "审批时间", prop: "last_modification_time" },
         { type: "text", label: "状态", prop: "status" },
+        { type: "text-btn", label: "附件", prop: "attachments", click: this.handleAttach, render_text_btn: row => {
+              return row.attachments[0] ? row.attachments[0].name : "";
+        }},
         { type: "text", label: "备注", prop: "remark", min_width: "200" }
-      ]
-    };
-  },
+      ],
+      attachmentsColumns: [
+        { type: 'text', label: '文件名称', prop: 'name' },
+        { type: 'text', label: '文件类型', prop: 'file_type' },
+        { type: 'text', label: '文件格式', prop: 'ext' },
+        { type: 'text', label: '文件大小', prop: 'size' },
+        { type: 'text', label: '上传人', prop: 'uploader', render_simple: 'name', },
+        { type: 'text', label: '上传时间', prop: 'creation_time' },
+        { type: 'action',
+          width: '178',
+          btns: [
+            { type: 'download', text: '下载', click: ({downloadUrl})=>{window.open(downloadUrl)} },
+            { type: 'view', text: '查看', click: ({viewUrl})=>{window.open(viewUrl)} },
+          ]
+        }
+    ]
+  };
+},
   computed: {
     taskId() {
       return this.row.task.id;
@@ -32,7 +57,11 @@ export default {
   },
   methods: {
     ...mapActions([]),
-    refreshData() {
+    handleAttach (row) {
+      this.dialogVisible = true;
+      this.attachments = row.attachments;
+    }, 
+    refreshData () {
       this.loading = true;
       const url = `/processes/${this.row.id}`;
       const success = _ => {
