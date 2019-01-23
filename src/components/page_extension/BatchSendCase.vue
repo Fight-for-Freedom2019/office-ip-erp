bc9f1291fe83886ac50a9c7e4928d79f47850d60
Linxi <lx02321@163.com>
2019-01-23 10:55:05 +0800

    批量派案

---
+++ src/components/page_extension/BatchSendCase.vue
@@ -0,0 +1,79 @@
+<!-- 批量派案 -->
+<template>
+  <el-form ref="form" :model="form" :rules="rules" label-width="120px" label-position="right">
+    <el-form-item label="代理人" prop="agent">
+      <remote-select type="agent" v-model="form.agent"></remote-select>
+    </el-form-item>
+    <el-form-item label="初审人" prop="first_reviewer">
+      <remote-select type="user" v-model="form.first_reviewer"></remote-select>
+    </el-form-item>
+    <el-form-item label="复审人">
+      <remote-select type="user" v-model="form.final_reviewer" add-type="user"></remote-select>
+    </el-form-item>
+    <el-form-item label="对外代理人">
+      <remote-select type="user" v-model="form.representative" add-type="user"></remote-select>
+    </el-form-item>
+    <el-form-item label="点数" prop="points">
+      <el-input type="text" placeholder="请输入点数" v-model.number="form.points"></el-input>
+      <span>注意此点数是原始点数，实际点数需要计算成熟度</span>
+    </el-form-item>
+    <el-form-item label="备注" prop="remark">
+      <el-input type="text" placeholder="请输入备注" v-model="form.remark"></el-input>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+  export default {
+    name: "BatchSendCase",
+    data() {
+      return {
+        form: {
+          agent: "",
+          first_reviewer: "",
+          final_reviewer: "",
+          representative: "",
+          points: "",
+          remark: "",
+        },
+        visible: false,
+        rules: {
+          agent: [
+            {required: true,message:"请选择代理人"}
+          ],
+        },
+      }
+    },
+    props: {
+      ids: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    methods: {
+      show() {
+        this.visible = true;
+      },
+      save(){
+        this.$refs.form.validate((valid)=>{
+          if(valid){
+            this.form.ids = this.ids;
+            const success = _ =>{
+              this.$message({type:"success",message:"操作成功!"})
+            }
+            this.$axiosPost({url:"/tasks/assign",data:this.form,success});
+          }else {
+            this.$message({type:"warning",message:"请选择必选项!"})
+          }
+        })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/page_extension/BatchTaskCommon.vue
@@ -0,0 +1,62 @@
+<!-- TaskCommon中所有批量操作 -->
+<template>
+  <div>
+    <app-shrink :visible.sync="visible" :title="title">
+      <span slot="header" style="float: right;">
+      <app-button-loading :func="save" ref="loadingBtn" text="保存"></app-button-loading>
+    </span>
+      <batch-send-case ref="sendCase" :ids="ids"></batch-send-case>
+    </app-shrink>
+  </div>
+</template>
+
+<script>
+  import BatchSendCase from "@/components/page_extension/BatchSendCase";
+
+  const constType = [
+    ["sendCase", "批量派案"],
+  ];
+  export default {
+    name: "BatchTaskCommon",
+    data() {
+      return {
+        visible: false,
+        constTypeMap: new Map(constType),
+      }
+    },
+    props: {
+      ids: {
+        type: Array,
+        default() {
+          return []
+        },
+      },
+      operationType: {
+        type: String,
+        default() {
+          return ""
+        },
+      },
+    },
+    computed: {
+      title: function () {
+        return this.constTypeMap.get(this.operationType);
+      }
+    },
+    methods: {
+      show() {
+        this.visible = true;
+      },
+      save() {
+        this.$refs[this.operationType].save();
+      }
+    },
+    components: {
+      BatchSendCase
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
