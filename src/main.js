b6b355167d478b6b6954126e61c1c9cfc8c052dc
Linxi <lx02321@163.com>
2019-01-28 09:17:20 +0800

    cpc文本编辑器修改

--- src/main.js
+++ src/main.js
@@ -5,28 +5,24 @@ import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
 import formCreate from "form-create";
-// import VueQuillEditor from "vue-quill-editor";
+import VueQuillEditor from "vue-quill-editor";
 import MyAxios from "@/const/MyAxios";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App";
 import "./fonts/iconfont.css";
 import axios from "@/const/axios";
 import tool from "./const/tool";
-// import AppButtonLoading from "@/components/common/AppButtonLoading";
 import "./components/common/components"
 import "./components/form/components"
-// import '../static/jquery-3.2.1.js'
 
-// import '../static/UE/ueditor.config.js'
-// import '../static/UE/ueditor.all.min.js'
-// import '../static/UE/lang/zh-cn/zh-cn.js'
-// import '../static/UE/ueditor.parse.min.js'
-
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 Vue.prototype.$axios = axios;
 Vue.prototype.$tool = tool;
 
 Vue.use(ElementUI);
-// Vue.use(VueQuillEditor);
+Vue.use(VueQuillEditor);
 Vue.use(MyAxios);
 Vue.use(formCreate);
 
