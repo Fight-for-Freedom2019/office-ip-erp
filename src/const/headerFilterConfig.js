8c09bcc5e86fde453837d2b9b1f9b9afb20f7057
kevin <princecharmingthi@163.com>
2019-02-14 11:59:49 +0800

    点数表头筛选

--- src/const/headerFilterConfig.js
+++ src/const/headerFilterConfig.js
@@ -982,6 +982,15 @@ const map = new Map([
       { type: "date", id: "mail_date", name: "发文日" },
       { type: "date", id: "creation_time", name: "上传日" }
     ]
-  ]
+  ],
+  ["points",[
+    { type: "static_select", id: "project_type", name: "案件类型", map_type: "project_type" },
+    { type: "static_select", id: "subtype", name: "案件子类型", map_type: "subtype" },
+    { type: "static_select", id: "process", name: "管制事项", map_type: "process_definition", url: true },
+    { type: "static_select", id: "area", name: "国家/地区", map_type: "area",},
+    { type: "static_select", id: "bonus_type", name: "提成类型", map_type: "bonus_type",},
+    { type: "static_select", id: "status", name: "状态", map_type: "bonus_status",},
+    { type: "remote_select", id: "user", name: "用户", data_key: 'data', url: '/users'},
+  ]]
 ]);
 export { map };
