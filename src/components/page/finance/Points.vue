fa8aa5c5c99bf0af4852684df2880556cbbf9aa5
shawn <shawn@iwdoing.com>
2019-02-14 12:00:54 +0800

    点数

--- src/components/page/finance/Points.vue
+++ src/components/page/finance/Points.vue
@@ -1,130 +1,225 @@
 <!-- 收款账户 -->
 <template>
-    <div class="Points">
-        <table-component :tableOption="tableOption" :data="tableData" ref="table" @update="update" @refresh="refresh"
-                         @refreshTableData="refreshTableData"></table-component>
-        <app-shrink :visible.sync="isPanelVisible" :modal='false' :title="title">
-            <span slot="header" style="float: right;">
-                <!--<el-button type="primary" size="small" v-if="compileType === 'add'" @click="save('add')">新建</el-button>-->
-                <app-button-loading :func="save" v-if="compileType === 'add'" ref="loadingBtn"></app-button-loading>
-                <el-button type="primary" size="small" v-if="compileType === 'edit'"
-                           @click="save('edit')">保存</el-button>
-            </span>
-            <points-edit :type="compileType" :data = "rowData" ref="RuleAdd" @update="update" @refresh="refresh"></points-edit>
-        </app-shrink>
-    </div>
+  <div class="Points">
+    <table-component
+      :tableOption="tableOption"
+      :data="tableData"
+      ref="table"
+      @update="update"
+      @refresh="refresh"
+      @refreshTableData="refreshTableData"
+    ></table-component>
+    <app-shrink :visible.sync="isPanelVisible" :modal="false" :title="title">
+      <span slot="header" style="float: right;">
+        <!--<el-button type="primary" size="small" v-if="compileType === 'add'" @click="save('add')">新建</el-button>-->
+        <app-button-loading :func="save" v-if="compileType === 'add'" ref="loadingBtn"></app-button-loading>
+        <el-button
+          type="primary"
+          size="small"
+          v-if="compileType === 'edit'"
+          @click="save('edit')"
+        >保存</el-button>
+      </span>
+      <points-edit
+        :type="compileType"
+        :data="rowData"
+        ref="RuleAdd"
+        @update="update"
+        @refresh="refresh"
+      ></points-edit>
+    </app-shrink>
+  </div>
 </template>
 
 <script>
-    import TableComponent from '@/components/common/TableComponent'
-    import AppShrink from '@/components/common/AppShrink'
-    import PointsEdit from '@/components/page/finance/PointsEdit'
-    import TableMixins from '@/mixins/table-mixins'
-    import Config from "@/const/selectConfig"
-    import { mapActions } from 'vuex'
+import TableComponent from "@/components/common/TableComponent";
+import AppShrink from "@/components/common/AppShrink";
+import PointsEdit from "@/components/page/finance/PointsEdit";
+import TableMixins from "@/mixins/table-mixins";
+import Config from "@/const/selectConfig";
+import { mapActions } from "vuex";
 
-    const config = new Map(Config);
+const config = new Map(Config);
 
-    export default {
-        name: "Points",
-        mixins: [TableMixins],
-        data() {
-            return {
-                URL: "/bonuses",
-                tableOption: {
-                    'name': 'PointsList',
-                    'url': "/bonuses",
-                    'height': 'default',
-                    'highlightCurrentRow': true,
-                    'is_search': true,
-                    'is_list_filter': true,
-                    'list_type': 'points',
-                    'treeFilter': 'points',
-                    'search_placeholder': '案号、关键字',
-                    'rowClick': this.handleRowClick,
-                    'header_btn': [
-                        {type: 'add', click: this.add},
-                        {type: 'delete'},
-                        // {type: 'export'},
-                        {type: 'control'},
-                    ],
-                    'columns': [
-                        {type: 'selection'},
-                        {type: 'text', label: '案号', prop: 'serial', min_width: '150'},
-                        {type: 'text', label: '标题', prop: 'title', min_width: '150'},
-                        {type: 'text', label: '案件类型', prop: 'project_type',render_simple:'name', width: '100'},
-                        {type: 'text', label: '案件子类型', prop: 'subtype',render_simple:'name', width: '120'},
-                        {type: 'text', label: '国家/地区', prop: 'area',render_simple:'name', width: '100'},
-                        {type: 'text', label: '申请日', prop: 'application_date', width: '100'},
-                        {type: 'text', label: '申请号', prop: 'application_number', width: '100'},
-                        {type: 'text', label: '管制事项', prop: 'process',render_simple:'name', width: '120'},
-                        {type: 'text', label: '流程节点', prop: 'task',render_simple:'name', width: '120'},
-                        {type: 'array', label: '案件标签', prop: 'project_tags', min_width: '150',render: _=>_.map(_=>_.name), },
-                        {type: 'array', label: '管制事项标签', prop: 'process_tags', min_width: '150',render: _=>_.map(_=>_.name),},
-                        {type: 'text', label: '点数', prop: 'points', width: '100'},
-                        {type: 'text', label: '提成类型', prop: 'bonus_type',render_simple:'name', width: '100'},
-                        {type: 'text', label: '用户', prop: 'user',render_simple:'name', width: '100'},
-                        {type: 'text', label: '最后修改用户', prop: 'last_modifier_user',render_simple:'name', width: '100'},
-                        {type: 'text', label: '最后修改时间', prop: 'last_modification_time', width: '100'},
-                        {type: 'text', label: '状态', prop: 'status',render_simple:'name', width: '120'},
-                        {type: 'text', label: '备注', prop: 'remark', width: '100'},
-                    ],
-                },
-                compileType: "add",
-                isPanelVisible: false,
-                tableData: [],
-                rowID:null,
-                rowData:null,
-                title:"",
-            }
-        },
-        created() {
-            this.refreshTags();
-        },
-        methods: {
-            ...mapActions([
-            // 'refreshExtends', //extend-field
-            'refreshTags', //product
-            ]),
-            refreshTableData(option) {
-                const success = _ => {
-                    this.compileType === "add" ? this.closeVisible("isPanelVisible") : "";
-                    this.tableData = _.data;
-                };
-                const data = Object.assign({}, option);
-                this.$axiosGet({
-                    url: this.URL,
-                    data: data,
-                    success,
-                })
-            },
-            handleRowClick(row) {
-                this.rowData = row;
-                this.rowID = row.id;
-                this.openVisible("isPanelVisible");
-                this.compileType = "edit";
-                this.title = `编辑点数>${row.serial}-${row.title}-${row.process.name}`
-                
-            },
-            add() {
-                this.rowData = {};
-                this.title = "新增点数";
-                this.compileType = "add";
-                this.openVisible("isPanelVisible");
-                this.$refs.RuleAdd?this.$refs.RuleAdd.clear():"";
-            },
-            save(type) {
-                this.$refs.RuleAdd.submitForm(type,this.rowID)
-            },
-        },
-        components: {
-            TableComponent,
-            AppShrink,
-            PointsEdit,
-        },
+export default {
+  name: "Points",
+  mixins: [TableMixins],
+  data() {
+    return {
+      URL: "/bonuses",
+      tableOption: {
+        name: "PointsList",
+        url: "/bonuses",
+        height: "default",
+        highlightCurrentRow: true,
+        is_search: true,
+        is_list_filter: true,
+        list_type: "points",
+        treeFilter: "points",
+        search_placeholder: "案号、关键字",
+        rowClick: this.handleRowClick,
+        header_btn: [
+          { type: "add", click: this.add },
+          { type: "delete" },
+          // {type: 'export'},
+          { type: "control" }
+        ],
+        columns: [
+          { type: "selection" },
+          { type: "text", label: "案号", prop: "serial", min_width: "150" },
+          { type: "text", label: "标题", prop: "title", min_width: "150" },
+          {
+            type: "text",
+            label: "案件类型",
+            prop: "project_type",
+            render_simple: "name",
+            width: "90"
+          },
+          {
+            type: "text",
+            label: "案件子类型",
+            prop: "subtype",
+            render_simple: "name",
+            width: "100"
+          },
+          {
+            type: "text",
+            label: "国家/地区",
+            prop: "area",
+            render_simple: "name",
+            width: "90"
+          },
+          {
+            type: "text",
+            label: "管制事项",
+            prop: "process",
+            render_simple: "name",
+            width: "110"
+          },
+          {
+            type: "array",
+            label: "案件标签",
+            prop: "project_tags",
+            min_width: "120",
+            render: _ => _.map(_ => _.name)
+          },
+          {
+            type: "array",
+            label: "管制事项标签",
+            prop: "process_tags",
+            min_width: "120",
+            render: _ => _.map(_ => _.name)
+          },
+          {
+            type: "text",
+            label: "点数类型",
+            prop: "bonus_type",
+            render_simple: "name",
+            width: "80"
+          },
+          {
+            type: "text",
+            label: "点数对象",
+            prop: "user",
+            render_simple: "name",
+            width: "80"
+          },
+          { type: "text", label: "点数", prop: "points", width: "80" },
+          {
+            type: "text",
+            label: "状态",
+            prop: "status",
+            render_simple: "name",
+            width: "80"
+          },
+          {
+            type: "text",
+            label: "申请日",
+            prop: "application_date",
+            width: "100"
+          },
+          {
+            type: "text",
+            label: "申请号",
+            prop: "application_number",
+            width: "135"
+          },
+          {
+            type: "text",
+            label: "最后修改用户",
+            prop: "last_modifier_user",
+            render_simple: "name",
+            width: "100"
+          },
+          {
+            type: "text",
+            label: "最后修改时间",
+            prop: "last_modification_time",
+            width: "160"
+          },
+          {
+            type: "text",
+            label: "流程节点",
+            prop: "task",
+            render_simple: "name",
+            width: "80"
+          },
+          { type: "text", label: "备注", prop: "remark", width: "80" }
+        ]
+      },
+      compileType: "add",
+      isPanelVisible: false,
+      tableData: [],
+      rowID: null,
+      rowData: null,
+      title: ""
+    };
+  },
+  created() {
+    this.refreshTags();
+  },
+  methods: {
+    ...mapActions([
+      // 'refreshExtends', //extend-field
+      "refreshTags" //product
+    ]),
+    refreshTableData(option) {
+      const success = _ => {
+        this.compileType === "add" ? this.closeVisible("isPanelVisible") : "";
+        this.tableData = _.data;
+      };
+      const data = Object.assign({}, option);
+      this.$axiosGet({
+        url: this.URL,
+        data: data,
+        success
+      });
+    },
+    handleRowClick(row) {
+      this.rowData = row;
+      this.rowID = row.id;
+      this.openVisible("isPanelVisible");
+      this.compileType = "edit";
+      this.title = `编辑点数>${row.serial}-${row.title}-${row.process.name}`;
+    },
+    add() {
+      this.rowData = {};
+      this.title = "新增点数";
+      this.compileType = "add";
+      this.openVisible("isPanelVisible");
+      this.$refs.RuleAdd ? this.$refs.RuleAdd.clear() : "";
+    },
+    save(type) {
+      this.$refs.RuleAdd.submitForm(type, this.rowID);
     }
+  },
+  components: {
+    TableComponent,
+    AppShrink,
+    PointsEdit
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/components/page/setting/bonus/RuleAdd.vue
+++ src/components/page/setting/bonus/RuleAdd.vue
@@ -1,216 +1,231 @@
 <!-- 新增提成规则类型 -->
 <template>
-    <div class="main" style="margin-top:10px;">
-        <el-form label-width="120px" :model="form" :rules="rules" ref="form">
-            <el-form-item label="规则名称" prop="name">
-                <el-input type="text" placeholder="请输入规则名称" v-model="form.name"></el-input>
-            </el-form-item>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="案件类型" prop="project_type">
-                        <static-select type="project_type" v-model="form.project_type"></static-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="案件子类型" prop="project_subtype">
-                        <static-select type="subtype" v-model="form.project_subtype"></static-select>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="国家/地区" prop="area">
-                        <static-select type="country" v-model="form.area"></static-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="管制事项" prop="process_definition">
-                        <static-select type="process_definition" v-model="form.process_definition"></static-select>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="提成类型" prop="bonus_type">
-                        <static-select type="bonus_type" v-model="form.bonus_type"></static-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="生成时机" prop="timing">
-                        <static-select type="bonus_timing" v-model="form.timing"></static-select>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="点数" prop="points">
-                        <el-input type="text" placeholder="请输入点数" v-model="form.points"></el-input>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-form-item label="客户" prop="customer">
-                <remote-select type="customer" v-model="form.customer"></remote-select>
-            </el-form-item>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="案件标签" prop="project_tags">
-                        <static-select type="tags" v-model="form.project_tags" multiple></static-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="管制事项标签" prop="process_tags">
-                        <static-select type="tags" v-model="form.process_tags" multiple></static-select>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="优先级" prop="priority">
-                        <el-input type="text" placeholder="请输入优先级" v-model="form.priority"></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="流程节点" prop="process_action">
-                        <static-select type="actions" v-model="form.process_action"></static-select>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="备注" prop="remark">
-                        <el-input type="text" placeholder="请输入备注" v-model="form.remark"></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                    <el-form-item label="是否折扣" prop="is_discount">
-                        <app-switch type="is" v-model="form.is_discount"></app-switch>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-        </el-form>
-    </div>
+  <div class="main" style="margin-top:10px;">
+    <el-form label-width="120px" :model="form" :rules="rules" ref="form">
+      <el-form-item label="规则名称" prop="name">
+        <el-input type="text" placeholder="请输入规则名称" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="案件类型" prop="project_type">
+            <static-select type="project_type" v-model="form.project_type"></static-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="案件子类型" prop="project_subtype">
+            <static-select type="subtype" v-model="form.project_subtype"></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="国家/地区" prop="area">
+            <static-select type="country" v-model="form.area"></static-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="管制事项" prop="process_definition">
+            <static-select type="process_definition" v-model="form.process_definition"></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="提成类型" prop="bonus_type">
+            <static-select type="bonus_type" v-model="form.bonus_type"></static-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="生成时机" prop="timing">
+            <static-select type="bonus_timing" v-model="form.timing"></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="点数" prop="points">
+            <el-input type="text" placeholder="请输入点数" v-model="form.points"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="点数对象" prop="bonus_target">
+            <static-select type="bonus_target" v-model="form.bonus_target"></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item label="客户" prop="customer">
+        <remote-select type="customer" v-model="form.customer"></remote-select>
+      </el-form-item>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="案件标签" prop="project_tags">
+            <static-select type="tags" v-model="form.project_tags" multiple></static-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="管制事项标签" prop="process_tags">
+            <static-select type="tags" v-model="form.process_tags" multiple></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="优先级" prop="priority">
+            <el-input type="text" placeholder="请输入优先级" v-model="form.priority"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="流程节点" prop="process_action">
+            <static-select type="actions" v-model="form.process_action"></static-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="备注" prop="remark">
+            <el-input type="text" placeholder="请输入备注" v-model="form.remark"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="是否折扣" prop="is_discount">
+            <app-switch type="is" v-model="form.is_discount"></app-switch>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+  </div>
 </template>
 
 <script>
-    import RemoteSelect from "@/components/form/RemoteSelect";
-    import StaticSelect from "@/components/form/StaticSelect";
-    import AppSwitch from "@/components/form/AppSwitch";
-    import {mapActions} from 'vuex'
+import RemoteSelect from "@/components/form/RemoteSelect";
+import StaticSelect from "@/components/form/StaticSelect";
+import AppSwitch from "@/components/form/AppSwitch";
+import { mapActions } from "vuex";
 
-    export default {
-        name: "RuleAdd",
-        data() {
-            return {
-                URL: "/bonus_rules",
-                form: {
-                    name: "",
-                    customer: "",
-                    project_type: "",
-                    project_subtype: "",
-                    bonus_type: "",
-                    area: "",
-                    points: "",
-                    project_tags: "",
-                    process_tags:'',
-                    process_definition:'',
-                    process_action:'',
-                    timing:'',
-                    priority:'',
-                    is_discount:1,
-                    remark:'',
-                },
-                rules: {
-                    name: [
-                        {required: true, message: "请输入提成规则名称", trigger: "blur"}
-                    ],
-                    project_type: [
-                        {required: true, message: "请选择案件类型", trigger: "blur"}
-                    ],
-                    project_subtype: [
-                        {required: true, message: "请选择案件子类型", trigger: "blur"}
-                    ],
-                    area: [
-                        {required: true, message: "请选择案件国家/地区", trigger: "blur"}
-                    ],
-                    process_definition: [
-                        {required: true, message: "请选择管制事项", trigger: "blur"}
-                    ],
-                    bonus_type: [
-                        {required: true, message: "请选择提成类型", trigger: "blur"}
-                    ],
-                    timing: [
-                        {required: true, message: "请选择生成时机", trigger: "blur"}
-                    ],
-                    points: [
-                        {required: true, message: "请输入点数", trigger: "blur"}
-                    ],
-                },
-            }
-        },
-        props: {
-            data: Object,
-            type: String,
-        },
-        created() {
-            
-        },
-        mounted() {
-            this.coverObj(this.data);
-        },
-        watch: {
-            data: function (val, oldVal) {
-                this.coverObj(val);
-            },
-        },
-        methods: {
-            ...mapActions([
-                'initializeSelectorCache',
-            ]),
-            submitForm(type, id) {
-                this.$refs['form'].validate((valid) => {
-                    if (valid) {
-                        let url;
-                        let data;
-                        let msg;
-                        let fun;
-                        if (type === "add") {
-                            url = this.URL;
-                            data = this.form;
-                            msg = "创建";
-                            fun = "refresh"
-                        } else {
-                            url = `${this.URL}/${id}`;
-                            data = this.form;
-                            msg = "修改";
-                            fun = "update"
-                        }
-                        const success = _ => {
-                            this.$message({type: "success", message: `${msg}成功!`});
-                            this.$emit(fun);
-                        };
-                        type === "add" ? this.$axiosPost({url, data, success}) : this.$axiosPut({url, data, success})
-                    } else {
-                        this.$message({type: "warning", message: "请正确填写"});
-                    }
-                })
-            },
-            clear(){
-                this.$refs.form.resetFields();
-            },
-            coverObj(val){
-                val?this.$tool.coverObj(this.form,val,{obj:[
-                    'customer','project_type','project_subtype','bonus_type','timing','process_definition','process_action','area','project_tags','process_tags',
-                ]}):"";
-            },
-        },
-        components: {
-            RemoteSelect,
-            StaticSelect,
-            AppSwitch,
-        },
+export default {
+  name: "RuleAdd",
+  data() {
+    return {
+      URL: "/bonus_rules",
+      form: {
+        name: "",
+        customer: "",
+        project_type: "",
+        project_subtype: "",
+        bonus_type: "",
+        bonus_target: 1,
+        area: "",
+        points: "",
+        project_tags: "",
+        process_tags: "",
+        process_definition: "",
+        process_action: "",
+        timing: "",
+        priority: "",
+        is_discount: 1,
+        remark: ""
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入提成规则名称", trigger: "blur" }
+        ],
+        project_type: [
+          { required: true, message: "请选择案件类型", trigger: "blur" }
+        ],
+        project_subtype: [
+          { required: true, message: "请选择案件子类型", trigger: "blur" }
+        ],
+        area: [
+          { required: true, message: "请选择案件国家/地区", trigger: "blur" }
+        ],
+        process_definition: [
+          { required: true, message: "请选择管制事项", trigger: "blur" }
+        ],
+        bonus_type: [
+          { required: true, message: "请选择提成类型", trigger: "blur" }
+        ],
+        timing: [
+          { required: true, message: "请选择生成时机", trigger: "blur" }
+        ],
+        points: [{ required: true, message: "请输入点数", trigger: "blur" }]
+      }
+    };
+  },
+  props: {
+    data: Object,
+    type: String
+  },
+  created() {},
+  mounted() {
+    this.coverObj(this.data);
+  },
+  watch: {
+    data: function(val, oldVal) {
+      this.coverObj(val);
     }
+  },
+  methods: {
+    ...mapActions(["initializeSelectorCache"]),
+    submitForm(type, id) {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          let url;
+          let data;
+          let msg;
+          let fun;
+          if (type === "add") {
+            url = this.URL;
+            data = this.form;
+            msg = "创建";
+            fun = "refresh";
+          } else {
+            url = `${this.URL}/${id}`;
+            data = this.form;
+            msg = "修改";
+            fun = "update";
+          }
+          const success = _ => {
+            this.$message({ type: "success", message: `${msg}成功!` });
+            this.$emit(fun);
+          };
+          type === "add"
+            ? this.$axiosPost({ url, data, success })
+            : this.$axiosPut({ url, data, success });
+        } else {
+          this.$message({ type: "warning", message: "请正确填写" });
+        }
+      });
+    },
+    clear() {
+      this.$refs.form.resetFields();
+    },
+    coverObj(val) {
+      val
+        ? this.$tool.coverObj(this.form, val, {
+            obj: [
+              "customer",
+              "project_type",
+              "project_subtype",
+              "bonus_type",
+              "bonus_target",
+              "timing",
+              "process_definition",
+              "process_action",
+              "area",
+              "project_tags",
+              "process_tags"
+            ]
+          })
+        : "";
+    }
+  },
+  components: {
+    RemoteSelect,
+    StaticSelect,
+    AppSwitch
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
