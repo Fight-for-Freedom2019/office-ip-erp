686955d3812ea3644e181a622f7deba9ad00b159
shawn <shawn@iwdoing.com>
2019-02-25 15:54:56 +0800

    应收明细字段

--- src/components/page/RequestPayout.vue
+++ src/components/page/RequestPayout.vue
@@ -90,7 +90,10 @@ export default {
         merge: [
           { col: 2, key: "user.id", prop: "user" },
           { col: 3, key: "project.id", prop: "project.serial" },
-          { col: 4, key: "project.id", prop: "project.title" }
+          { col: 4, key: "project.id", prop: "project.title" },
+          { col: 11, key: "project.id", prop: "official_sum_currency" },
+          { col: 12, key: "project.id", prop: "service_sum_currency" },
+          { col: 13, key: "project.id", prop: "sum_currency" }
         ],
         search_placeholder: "",
         rowClick: this.handleRowClick,
@@ -125,9 +128,9 @@ export default {
           {
             type: "text",
             label: "客户",
-            prop: "user",
+            prop: "customer",
             render_simple: "name",
-            min_width: "178",
+            min_width: "120",
             render_header: true
           },
           {
@@ -141,27 +144,26 @@ export default {
             type: "text",
             label: "标题",
             prop: "project.title",
-            width: "200",
-            render_header: true
+            width: "180"
           },
 
           {
             type: "text",
             label: "费用名称",
-            prop: "fee_code.name",
-            width: "160",
+            prop: "fee_code",
+            render_simple: "name",
+            width: "130",
             render_header: true
           },
           {
             type: "text",
-            label: "费用类型",
-            prop: "fee_code",
-            width: "100",
-            render: (h, item) => {
-              return h("span", item ? item.fee_type.name : "");
-            },
+            label: "类型",
+            prop: "fee_type",
+            render_simple: "name",
+            width: "80",
             render_header: true
           },
+          // TODO 接口有字段缺失
           {
             type: "text",
             label: "金额",
@@ -173,10 +175,16 @@ export default {
             type: "text",
             label: "币别",
             prop: "currency",
-            width: "70",
-            render_header: true
+            width: "55"
+            // render_header: true
           },
-          { type: "text", label: "汇率", prop: "roe", width: "70" },
+          {
+            type: "text",
+            label: "汇率",
+            prop: "roe",
+            width: "68",
+            align: "right"
+          },
           {
             type: "text",
             label: "人民币",
@@ -186,17 +194,40 @@ export default {
           },
           {
             type: "text",
+            label: "官费小计",
+            prop: "official_sum_currency",
+            width: "100",
+            align: "right"
+          },
+          {
+            type: "text",
+            label: "代理费小计",
+            prop: "service_sum_currency",
+            width: "100",
+            align: "right"
+          },
+          {
+            type: "text",
+            label: "小计",
+            prop: "sum_currency",
+            width: "100",
+            align: "right"
+          },
+          {
+            type: "text",
             label: "申请号",
             prop: "project.application_number",
             width: "165",
-            render_header: true
+            render_header: true,
+            expanded: true
           },
           {
             type: "text",
             label: "申请日",
             prop: "project.application_date",
             width: "100",
-            render_header: true
+            render_header: true,
+            expanded: true
           },
           {
             type: "text",
@@ -270,10 +301,16 @@ export default {
         sumFunc: param => {
           const { columns, data } = param;
           const sums = [];
-          const fields = ["rmb_amount_currency", "amount_currency"];
+          const fields = [
+            "rmb_amount_currency",
+            "amount_currency",
+            "official_sum_currency",
+            "service_sum_currency",
+            "sum_currency"
+          ];
           columns.forEach((column, index) => {
             if (index === 0) {
-              sums[index] = "小计";
+              sums[index] = "总计";
               return;
             } else if (index === 1) {
               sums[index] = "";
