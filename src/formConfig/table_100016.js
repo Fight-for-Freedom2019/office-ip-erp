8814f7dba58f5c8f96aea17ca52882b681ca5b4e
Linxi <lx02321@163.com>
2019-02-26 16:26:06 +0800

    附件value格式修改

--- src/formConfig/table_100016.js
+++ src/formConfig/table_100016.js
@@ -2,16 +2,16 @@
 import {vm as change_reason_vm} from '../formTemplate/change-reason'
 import {vm as change_content_vm} from '../formTemplate/change-content-100016'
 
-const attachments = [{value: 1, label: '双方当事人签章的权利转移协议书'}, {value: 2, label: '全体权利人同意转让的证明材料'}, {value: 3, label: '全体权利人同意赠与的证明材料'}, {
-    value: 4,
+const attachments = [{value: {id:1,name:"双方当事人签章的权利转移协议书"}, label: '双方当事人签章的权利转移协议书'}, {value: {id:2,name:"全体权利人同意转让的证明材料"}, label: '全体权利人同意转让的证明材料'}, {value: {id:3,name:"全体权利人同意赠与的证明材料"}, label: '全体权利人同意赠与的证明材料'}, {
+    value: {id:4,name:"双方当事人签字或盖章的说明变更理由的证明文件"},
     label: '双方当事人签字或盖章的说明变更理由的证明文件'
-}, {value: 5, label: '上级主管部门或当地工商行政管理部门出具的变更名称的证明文件'}, {value: 6, label: '户籍管理部门出具的更改姓名的证明文件'}, {
-    value: 7,
+}, {value: {id:5,name:"上级主管部门或当地工商行政管理部门出具的变更名称的证明文件"}, label: '上级主管部门或当地工商行政管理部门出具的变更名称的证明文件'}, {value: {id:6,name:'户籍管理部门出具的更改姓名的证明文件'}, label: '户籍管理部门出具的更改姓名的证明文件'}, {
+    value: {id:7,name:'公证机关证明继承人合法地位的公证书'},
     label: '公证机关证明继承人合法地位的公证书'
-}, {value: 8, label: '地方知识产权管理部门的调解书'}, {value: 9, label: '人民法院的判决书或者调解书'}, {value: 10, label: '国务院商务主管部门出具的证明文件'}, {
-    value: 11,
+}, {value: {id:8,name:'地方知识产权管理部门的调解书'}, label: '地方知识产权管理部门的调解书'}, {value: {id:9,name:'人民法院的判决书或者调解书'}, label: '人民法院的判决书或者调解书'}, {value: {id:10,name:'国务院商务主管部门出具的证明文件'}, label: '国务院商务主管部门出具的证明文件'}, {
+    value: {id:11,name:'变更后申请人或者专利权人的专利代理委托书'},
     label: '变更后申请人或者专利权人的专利代理委托书'
-}, {value: 12, label: '公证材料'}, {value: 13, label: '其他附件'}]
+}, {value: {id:12,name:'公证材料'}, label: '公证材料'}, {value: {id:13,name:'其他附件'}, label: '其他附件'}]
 //change-reason
 let rule = [
     {
@@ -57,6 +57,7 @@ let rule = [
         props:{
             multiple: true,
             filterable: true,
+            "value-key":"id"
         },
     },
 ]
