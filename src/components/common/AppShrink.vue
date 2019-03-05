906df7d9262506e028e7c0ba9fb52339d4e4e322
kevin <princecharmingthi@163.com>
2019-03-05 09:27:03 +0800

    调整面板标题宽度

--- src/components/common/AppShrink.vue
+++ src/components/common/AppShrink.vue
@@ -4,7 +4,7 @@
 	<transition name="slide-fade">
 	<div :style="shirnkStyle" class="app-shrink" v-show="visible">
 		<div class="app-shrink-head">
-			<span style="font-size: 18px; font-weight: bold;float: left;max-width: 430px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;" :title="title" class="fs">{{ title }}</span>
+			<span style="font-size: 18px; font-weight: bold;float: left;max-width: 300px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;" :title="title" class="fs">{{ title }}</span>
 			<el-button v-if="isClose" icon="el-icon-close" style="float: right; border: 0; height: 40px;" @click="close" title="关闭"></el-button>
 			<slot name="header"></slot>
 		</div>
