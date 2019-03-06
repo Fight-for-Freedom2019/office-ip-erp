3070ab97798668f5a28d50eece979549cd91a91f
Linxi <lx02321@163.com>
2019-03-06 16:02:43 +0800

    修复复审请求口头审理通知书回执

--- src/formTemplate/invalid-receipt.js
+++ src/formTemplate/invalid-receipt.js
@@ -3,7 +3,7 @@ import {handlePadding, handleSingle} from '../formConfig/handle/handle'
 function vm(label = "") {
     const template = `
         <div class="custom-checkbox">
-            <el-checkbox @change="change('oral_exam_join')" v-model="extendData.oral_exam_join" label="oral_exam_join">参加<el-date-picker value-format="yyyy-MM-dd" placeholder="日期" v-model="extendData.oral_exam_date"></el-date-picker>日<el-input v-model="extendData.oral_exam_hour" placeholder="时间"></el-input>时举行的口头审理</el-checkbox>
+            <el-checkbox @change="change('oral_exam_join')" v-model="extendData.oral_exam_join" label="oral_exam_join">参加<el-date-picker style="width: auto" value-format="yyyy-MM-dd" placeholder="日期" v-model="extendData.oral_exam_date"></el-date-picker>日<el-input v-model="extendData.oral_exam_hour" placeholder="时间"></el-input>时举行的口头审理</el-checkbox>
             <el-checkbox @change="change('oral_exam_not_join')" v-model="extendData.oral_exam_not_join" label="notice_supplemental">不能参加口头审理</el-checkbox>
         </div>
     `;
@@ -13,6 +13,7 @@ function vm(label = "") {
                 oral_exam_join: false,
                 oral_exam_not_join: false,
                 oral_exam_date: "",
+                oral_exam_hour:"",
 
             },
             checked: "",
