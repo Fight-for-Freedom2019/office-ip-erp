0c887fe027e5591b4444b477e3258bffa027db19
Linxi <lx02321@163.com>
2019-03-06 14:18:15 +0800

    同上

--- src/formConfig/table_100108.js
+++ src/formConfig/table_100108.js
@@ -2,14 +2,17 @@
 import {vm as upload_vm} from '../formTemplate/Upload'
 
 let rule = [
-    {
-        type: "input",title: "申请人",field: "patent_number",value: "",
-    },
-    upload_vm({label:"图片",url:"/url",tip:"上传图片",type:"custom",field:"files",common:true}),
+  {
+    type: "input", title: "申请号", field: "patent_number", value: "",
+  },
+  {
+    type: "hidden", field: "type", value: "form",
+  },
+  upload_vm({label: "图片", url: "/url", tip: "上传图片", type: "custom", field: "pictures", common: true}),
 
 ]
 const content = {
-    rule    // rule字段名称不能更改
+  rule    // rule字段名称不能更改
 }
 
 export default content
