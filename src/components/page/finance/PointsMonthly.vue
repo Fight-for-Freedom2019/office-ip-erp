5bacb15a51bd1a5c94b65dea3a6a4059bc9c9b95
shawn <shawn@iwdoing.com>
2019-03-06 09:40:20 +0800

    点数编辑功能

--- src/components/page/finance/PointsMonthly.vue
+++ src/components/page/finance/PointsMonthly.vue
@@ -9,7 +9,7 @@
       @refresh="refresh"
       @refreshTableData="refreshTableData"
     ></table-component>
-    <points-monthly-detail ref="details"></points-monthly-detail>
+    <points-monthly-detail ref="details" @update="update"></points-monthly-detail>
   </div>
 </template>
 
@@ -21,88 +21,89 @@ import PointsMonthlyDetail from "@/components/page_extension/PointsMonthlyDetail
 import { mapActions } from "vuex";
 
 export default {
-  name: "BonusReport",
-  mixins: [TableMixins],
-  data() {
-    return {
-      URL: "/bonus_reports",
-      tableOption: {
-        name: "BonusReportList",
-        url: "/bonus_reports",
-        height: "default",
-        highlightCurrentRow: true,
-        is_search: true,
-        is_list_filter: true,
-        list_type: "bonus_report",
-        treeFilter: "bonus_report",
-        search_placeholder: "用户名称、月份",
-        'rowClick': this.handleRowClick,
-        header_btn: [
-          // {type: 'add', click: this.add},
-          // {type: 'delete'},
-          // {type: 'export'},
-          { type: "control" }
-        ],
-        columns: [
-          { type: "selection" },
-          {
-            type: "text",
-            label: "用户",
-            prop: "user",
-            render_simple: "name",
-            min_width: "150",
-            render_header: true
-          },
-          {
-            type: "text",
-            label: "月份",
-            prop: "month",
-            min_width: "100",
-            render_header: true
-          },
-          { type: "text", label: "总点数", prop: "points", min_width: "100" },
-          // {type: 'text', label: '绩效点数', prop: 'points_bonus', min_width: '100'},
-          // {type: 'text', label: '总点数', prop: 'sum', min_width: '100'},
-          { type: "text", label: "月度排名", prop: "rank", min_width: "100" },
-          { type: "text", label: "备注", prop: "remark", min_width: "100" }
-        ]
-      },
-      compileType: "add",
-      tableData: [],
-      rowID: null,
-      rowData: null,
-      title: ""
-    };
-  },
-  created() {
-    this.refreshTags();
-  },
-  methods: {
-    ...mapActions([
-      // 'refreshExtends', //extend-field
-      "refreshTags" //product
-    ]),
-    refreshTableData(option) {
-      const success = _ => {
-        this.compileType === "add" ? this.closeVisible("isPanelVisible") : "";
-        this.tableData = _.data;
-      };
-      const data = Object.assign({}, option);
-      this.$axiosGet({
-        url: this.URL,
-        data: data,
-        success
-      });
+    name: "BonusReport",
+    mixins: [TableMixins],
+    data() {
+        return {
+            URL: "/bonus_reports",
+            tableOption: {
+                name: "BonusReportList",
+                url: "/bonus_reports",
+                height: "default",
+                highlightCurrentRow: true,
+                is_search: true,
+                is_list_filter: true,
+                list_type: "bonus_report",
+                treeFilter: "bonus_report",
+                search_placeholder: "用户名称、月份",
+                'rowClick': this.handleRowClick,
+                header_btn: [
+                    // {type: 'add', click: this.add},
+                    // {type: 'delete'},
+                    // {type: 'export'},
+                    { type: "control" }
+                ],
+                columns: [
+                    { type: "selection" },
+                    {
+                        type: "text",
+                        label: "用户",
+                        prop: "user",
+                        render_simple: "name",
+                        min_width: "150",
+                        render_header: true
+                    },
+                    {
+                        type: "text",
+                        label: "月份",
+                        prop: "month",
+                        min_width: "100",
+                        render_header: true
+                    },
+                    { type: "text", label: "总点数", prop: "points", min_width: "100" },
+                    // {type: 'text', label: '绩效点数', prop: 'points_bonus', min_width: '100'},
+                    // {type: 'text', label: '总点数', prop: 'sum', min_width: '100'},
+                    { type: "text", label: "月度排名", prop: "rank", min_width: "100" },
+                    { type: "text", label: "备注", prop: "remark", min_width: "100" }
+                ]
+            },
+            compileType: "add",
+            tableData: [],
+            rowID: null,
+            rowData: null,
+            title: ""
+        };
     },
-    handleRowClick(row) {
-      this.$refs.details.show('edit', row.id);
+    created() {
+        this.refreshTags();
     },
-  },
-  components: {
-    TableComponent,
-    AppShrink,
-    PointsMonthlyDetail
-  }
+    methods: {
+        ...mapActions([
+            // 'refreshExtends', //extend-field
+            "refreshTags" //product
+        ]),
+        refreshTableData(option) {
+            const success = _ => {
+                this.compileType === "add" ? this.closeVisible("isPanelVisible") : "";
+                this.tableData = _.data;
+            };
+            const data = Object.assign({}, option);
+            this.$axiosGet({
+                url: this.URL,
+                data: data,
+                success
+            });
+        },
+        handleRowClick(row) {
+            this.$refs.details.show('edit', row.id);
+        },
+    },
+    components: {
+        TableComponent,
+        AppShrink,
+        PointsMonthlyDetail,
+
+    }
 };
 </script>
 
--- src/components/page_extension/PointsMonthlyDetail.vue
+++ src/components/page_extension/PointsMonthlyDetail.vue
@@ -2,7 +2,7 @@
 <template>
   <div>
     <app-shrink :visible.sync="visible" title="点数月报详情">
-      <el-form :model="form" class="form-information">
+      <el-form :model="form" class="form-information"  v-loading="loading" element-loading-text="数据加载中">
         <el-row>
           <el-col :span="8">
             <el-form-item label="用户" class="form-item-text">{{ form.user }}</el-form-item>
@@ -22,115 +22,91 @@
             <el-form-item label="备注" class="form-item-text">{{ form.remark }}</el-form-item>
           </el-col>
         </el-row>    
+        <app-table :data="tableData" :columns="columns" height="700" ref="table"></app-table>   
       </el-form> 
-      <app-table :data="tableData" :columns="columns" height="700"  ref="table"></app-table>   
     </app-shrink>
+    <points-edit ref="PointsEdit" @update="update"></points-edit>
   </div>
 </template>
 <script>
-const  URL = '/bonus_reports';
+const URL = '/bonus_reports';
+
+import PointsEdit from "@/components/page/finance/PointsEdit";
+
 export default {
-  name: 'pointsMonthlyDetail',
-  data() {
-    return {
-      visible: false,  
-      form: {
-        user: '',
-        month: '',
-        points: '',
-        rank: '',
-        remark: '',   
-      },
-      columns: [
-         { type: "text", label: "案号", prop: "serial", min_width: "150" },
-          { type: "text", label: "标题", prop: "title", min_width: "200" },
-          {
-            type: "text",
-            label: "案件类型",
-            prop: "project_type",
-            render_simple: "name",
-            width: "90",
-          },
-          {
-            type: "text",
-            label: "案件子类型",
-            prop: "subtype",
-            render_simple: "name",
-            width: "110",
-          },
-          {
-            type: "text",
-            label: "国家/地区",
-            prop: "area",
-            render_simple: "name",
-            width: "100",
-          },
-          {
-            type: "text",
-            label: "管制事项",
-            prop: "process",
-            render_simple: "name",
-            width: "120",
-          },
-          { type: "text", label: "点数", prop: "points", width: "80" },
-          {
-            type: "array",
-            label: "案件标签",
-            prop: "project_tags",
-            min_width: "120",
-            render: _ => _.map(_ => _.name)
-          },
-          {
-            type: "array",
-            label: "管制事项标签",
-            prop: "process_tags",
-            min_width: "120",
-            render: _ => _.map(_ => _.name)
-          },
-          {
-            type: "text",
-            label: "点数类型",
-            prop: "bonus_type",
-            render_simple: "name",
-            width: "110",
-          },
-          
-          {
-            type: "text",
-            label: "用户",
-            prop: "user",
-            render_simple: "name",
-            width: "80",
-          },
-          {
-            type: "text",
-            label: "状态",
-            prop: "status",
-            render_simple: "name",
-            width: "80",
-          },
-      ],
-      tableData: [],
-    }
-  },
-  methods: {
-    show(type,id) {
-      this.visible = true;
-      this.getDetail(id)
+    name: 'PointsMonthlyDetail',
+    data() {
+        return {
+            visible: false,
+            form: {
+                user: '',
+                month: '',
+                points: '',
+                rank: '',
+                remark: '',
+            },
+            columns: [
+                { type: "text", label: "案号", prop: "serial", min_width: "150" },
+                { type: "text", label: "标题", prop: "title", min_width: "200" },
+                { type: "text", label: "案件类型", prop: "project_type", render_simple: "name", width: "90", },
+                { type: "text", label: "案件子类型", prop: "subtype", render_simple: "name", width: "110", },
+                { type: "text", label: "国家/地区", prop: "area", render_simple: "name", width: "100", },
+                { type: "text", label: "管制事项", prop: "process", render_simple: "name", width: "120", },
+                { type: "text", label: "点数", prop: "points", width: "80" },
+                { type: "array", label: "案件标签", prop: "project_tags", min_width: "120", render: _ => _.map(_ => _.name) },
+                { type: "array", label: "管制事项标签", prop: "process_tags", min_width: "120", render: _ => _.map(_ => _.name) },
+                { type: "text", label: "点数类型", prop: "bonus_type", render_simple: "name", width: "110", },
+                { type: "text", label: "用户", prop: "user", render_simple: "name", width: "80", },
+                { type: "text", label: "状态", prop: "status", render_simple: "name", width: "80", },
+                { type: "action", width: "100", fixed: "right", btns: [{ type: "edit", click: this.handleEdit }, { type: "delete", click: this.handleDelete }] },
+            ],
+            tableData: [],
+            id: 0,
+            loading: false,
+            mode: 'edit',
+        }
     },
-    getDetail (id) {
-      const url = `${URL}/${id}`;
-      const success = _=>{
-        const data = _.bonus_report;
-        this.tableData = data.detail;
-        this.$nextTick(()=>{
-          this.$tool.coverObj(this.form, data,);
-          this.form.user = data.user.name ?data.user.name:data.user;
-        })
-      };
-      this.$axiosGet({url, success});
-    }    
-  },
+    methods: {
+        show(mode, id) {
+            this.mode = mode;
+            this.id = id;
+            this.visible = true;
+            this.getDetail(id)
+        },
+        update() {
+            this.getDetail(this.id);
+            this.$emit('update');
+        },
+        handleEdit(row) {
+            console.log(row);
+            this.$refs.PointsEdit.show('edit', row)
+        },
+        handleDelete({ id }) {
+            const url = `/bonuses/${id}`;
+            const success = _ => {
+                this.show('edit', this.id)
+                this.$emit("update");
+            };
+            this.$axiosDelete({ url, success });
+        },
+        getDetail(id) {
+            this.loading = true;
+            const url = `${URL}/${id}`;
+            const success = _ => {
+                const data = _.bonus_report;
+                this.tableData = data.detail;
+                this.$nextTick(() => {
+                    this.$tool.coverObj(this.form, data, );
+                    this.form.user = data.user.name ? data.user.name : data.user;
+                })
+                this.loading = false;
+            };
+            this.$axiosGet({ url, success });
+        }
+    },
+    components: {
+        PointsEdit
+    }
 }
 </script>
 <style lang="scss" scoped>
