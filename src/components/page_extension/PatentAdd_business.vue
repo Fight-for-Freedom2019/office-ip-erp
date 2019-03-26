287002106ed4132dd367fe07f1304b4c2fa2c6fa
shawn <shawn@iwdoing.com>
2019-03-26 15:20:54 +0800

    专利立案页面IPR可选

--- src/components/page_extension/PatentAdd_business.vue
+++ src/components/page_extension/PatentAdd_business.vue
@@ -59,208 +59,208 @@ import JumpSelect from "@/components/form/JumpSelect";
 import FeeList from "@/components/form/FeeList";
 
 export default {
-  name: "patentAddBusiness",
-  props: {
-    type: String,
-    customer: [Number,String]
-  },
-  data() {
-    return {
-      contractObj: {},
-      form: {
-        customer: [],
-        ipr: "",
-        contact: "",
-        service: "",
-        customer_serial: "",
-        contract_type: 1,
-        contracts: [],
-        fee_policy: 1,
-        fees: [],
-        order: ""
-      },
-      fees: [],
-      rules: {
-        customer: {
-          type: "number",
-          required: true,
-          message: "客户不能为空",
-          trigger: "change"
-        },
-        ipr: {
-          type: "number",
-          required: true,
-          message: "IPR不能为空",
-          trigger: "change"
-        },
-        contact: {
-          type: "number",
-          required: true,
-          message: "技术联系人不能为空",
-          trigger: "change"
-        },
-        service: {
-          type: "number",
-          required: true,
-          message: "服务类型不能为空",
-          trigger: "change"
-        },
-        contract_type: {
-          type: "number",
-          required: true,
-          message: "合同类型不能为空",
-          trigger: "change"
-        },
-        fee_policy: {
-          type: "number",
-          required: true,
-          message: "费用策略不能为空",
-          trigger: "change"
-        },
-        fees: {
-          type: "array",
-          required: true,
-          message: "费用清单不能为空",
-          trigger: "change"
+    name: "patentAddBusiness",
+    props: {
+        type: String,
+        customer: [Number, String]
+    },
+    data() {
+        return {
+            contractObj: {},
+            form: {
+                customer: [],
+                ipr: "",
+                contact: "",
+                service: "",
+                customer_serial: "",
+                contract_type: 1,
+                contracts: [],
+                fee_policy: 1,
+                fees: [],
+                order: ""
+            },
+            fees: [],
+            rules: {
+                customer: {
+                    type: "number",
+                    required: true,
+                    message: "客户不能为空",
+                    trigger: "change"
+                },
+                ipr: {
+                    type: "number",
+                    required: false,
+                    message: "IPR不能为空",
+                    trigger: "change"
+                },
+                contact: {
+                    type: "number",
+                    required: true,
+                    message: "技术联系人不能为空",
+                    trigger: "change"
+                },
+                service: {
+                    type: "number",
+                    required: true,
+                    message: "服务类型不能为空",
+                    trigger: "change"
+                },
+                contract_type: {
+                    type: "number",
+                    required: true,
+                    message: "合同类型不能为空",
+                    trigger: "change"
+                },
+                fee_policy: {
+                    type: "number",
+                    required: true,
+                    message: "费用策略不能为空",
+                    trigger: "change"
+                },
+                fees: {
+                    type: "array",
+                    required: true,
+                    message: "费用清单不能为空",
+                    trigger: "change"
+                }
+            }
+        };
+    },
+    computed: {
+        customerParam() {
+            return this.customer ? { customer: this.customer } : null;
         }
-      }
-    };
-  },
-  computed: {
-    customerParam() {
-      return this.customer ? { customer: this.customer } : null;
-    }
-  },
-  methods: {
-    customerChanged() {
-      this.$emit("customerChanged", this.form.customer);
     },
-    setForm(data) {
-      this.$tool.coverObj(this.form, data, {
-        obj: ["service", "contract_type", "fee_policy", "contract"]
-      });
-    },
-    submitForm() {
-      return this.$tool.shallowCopy(this.form, {});
-    },
-    checkForm(callback) {
-      let flag = true;
-      this.$refs.form.validate(_ => {
-        flag = _;
-        callback(flag);
-      });
-    },
-    loadDefaultFees() {
-      if (!(this.form.customer && this.form.service)) {
-        return;
-      }
-      if (this.form.customer == "") {
-        return;
-      }
+    methods: {
+        customerChanged() {
+            this.$emit("customerChanged", this.form.customer);
+        },
+        setForm(data) {
+            this.$tool.coverObj(this.form, data, {
+                obj: ["service", "contract_type", "fee_policy", "contract"]
+            });
+        },
+        submitForm() {
+            return this.$tool.shallowCopy(this.form, {});
+        },
+        checkForm(callback) {
+            let flag = true;
+            this.$refs.form.validate(_ => {
+                flag = _;
+                callback(flag);
+            });
+        },
+        loadDefaultFees() {
+            if (!(this.form.customer && this.form.service)) {
+                return;
+            }
+            if (this.form.customer == "") {
+                return;
+            }
 
-      const url =
-        "/quotations?customer=" +
-        this.form.customer +
-        "&service=" +
-        this.form.service;
-      const success = _ => {
-        //有客户报价
-        if (_.data.data.length > 0) {
-          this.form.fees = [];
-          const fee = _.data.data[0];
-          if (fee.service_fee > 0) {
-            this.form.fees.push({
-              fee_type: _.data.service_fee_code,
-              amount: fee.service_fee,
-              currency: fee.service_fee_currency,
-              request_timing: 1
+            const url =
+                "/quotations?customer=" +
+                this.form.customer +
+                "&service=" +
+                this.form.service;
+            const success = _ => {
+                //有客户报价
+                if (_.data.data.length > 0) {
+                    this.form.fees = [];
+                    const fee = _.data.data[0];
+                    if (fee.service_fee > 0) {
+                        this.form.fees.push({
+                            fee_type: _.data.service_fee_code,
+                            amount: fee.service_fee,
+                            currency: fee.service_fee_currency,
+                            request_timing: 1
+                        });
+                    }
+                    if (fee.official_fee > 0) {
+                        this.form.fees.push({
+                            fee_type: _.data.official_fee_code,
+                            amount: fee.official_fee,
+                            currency: fee.official_fee_currency,
+                            request_timing: 1
+                        });
+                    }
+                } else {
+                    this.loadDefaultServiceQuotation();
+                }
+            };
+            this.$axiosGet({
+                url: url,
+                success
             });
-          }
-          if (fee.official_fee > 0) {
-            this.form.fees.push({
-              fee_type: _.data.official_fee_code,
-              amount: fee.official_fee,
-              currency: fee.official_fee_currency,
-              request_timing: 1
+        },
+        loadDefaultServiceQuotation() {
+            const url = "/services/" + this.form.service;
+            const success = _ => {
+                if (_.status) {
+                    this.form.fees = [];
+                    const fee = _.services;
+                    if (fee.service_fee > 0) {
+                        this.form.fees.push({
+                            fee_type: fee.service_fee_code,
+                            amount: fee.service_fee,
+                            currency: fee.service_fee_currency,
+                            request_timing: 1
+                        });
+                    }
+                    if (fee.official_fee > 0) {
+                        this.form.fees.push({
+                            fee_type: fee.official_fee_code,
+                            amount: fee.official_fee,
+                            currency: fee.official_fee_currency,
+                            request_timing: 1
+                        });
+                    }
+                }
+            };
+            this.$axiosGet({
+                url: url,
+                success
             });
-          }
-        } else {
-          this.loadDefaultServiceQuotation();
         }
-      };
-      this.$axiosGet({
-        url: url,
-        success
-      });
     },
-    loadDefaultServiceQuotation() {
-      const url = "/services/" + this.form.service;
-      const success = _ => {
-        if (_.status) {
-          this.form.fees = [];
-          const fee = _.services;
-          if (fee.service_fee > 0) {
-            this.form.fees.push({
-              fee_type: fee.service_fee_code,
-              amount: fee.service_fee,
-              currency: fee.service_fee_currency,
-              request_timing: 1
-            });
-          }
-          if (fee.official_fee > 0) {
-            this.form.fees.push({
-              fee_type: fee.official_fee_code,
-              amount: fee.official_fee,
-              currency: fee.official_fee_currency,
-              request_timing: 1
-            });
-          }
+    watch: {
+        // 'form.customer': {
+        //   handler(val) {
+        //       if(val && val.length != 0) {
+        //         if(val[0].contract_type) {
+        //           this.form.contract_type = val[0].contract_type;
+        //         }
+        //         if(val[0].fee_policy) {
+        //           this.form.fee_policy = val[0].fee_policy;
+        //         }
+        //       }
+        //   },
+        //   deep: true,
+        // },
+        "form.contract_type": {
+            handler(val) {
+                const obj = {};
+                obj.type = val;
+                this.contractObj = obj;
+            }
+        },
+        "form.service": {
+            handler(val) {
+                this.loadDefaultFees();
+            }
+        },
+        "form.customer": {
+            handler(val) {
+                this.loadDefaultFees();
+            }
         }
-      };
-      this.$axiosGet({
-        url: url,
-        success
-      });
-    }
-  },
-  watch: {
-    // 'form.customer': {
-    //   handler(val) {
-    //       if(val && val.length != 0) {
-    //         if(val[0].contract_type) {
-    //           this.form.contract_type = val[0].contract_type;
-    //         }
-    //         if(val[0].fee_policy) {
-    //           this.form.fee_policy = val[0].fee_policy;
-    //         }
-    //       }
-    //   },
-    //   deep: true,
-    // },
-    "form.contract_type": {
-      handler(val) {
-        const obj = {};
-        obj.type = val;
-        this.contractObj = obj;
-      }
     },
-    "form.service": {
-      handler(val) {
-        this.loadDefaultFees();
-      }
-    },
-    "form.customer": {
-      handler(val) {
-        this.loadDefaultFees();
-      }
+    components: {
+        StaticSelect,
+        RemoteSelect,
+        JumpSelect,
+        FeeList
     }
-  },
-  components: {
-    StaticSelect,
-    RemoteSelect,
-    JumpSelect,
-    FeeList
-  }
 };
 </script>
 
